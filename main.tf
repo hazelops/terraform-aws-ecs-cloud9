7687594f9c8094bf762c00e461591a484f861511
Dmitry Kireev <dmitry@hazelops.com>
2020-05-07 22:54:56 +0200

    Initial

---
+++ main.tf
@@ -0,0 +1,62 @@
+data "aws_region" "current" {}
+
+locals {
+  secret_names = concat(var.secret_names, [
+    PASSWORD
+  ])
+
+  environment = merge(var.environment,
+    {
+      PUID: "1000"
+      PGID: "1000"
+      TZ: "America/Los_Angeles"
+      USERNAME: "admin"
+      ECS_FARGATE = var.ecs_launch_type == "FARGATE" ? "true" : "false"
+    }
+  )
+
+  container_definition = {
+      name              = var.name
+      image = "${var.docker_image_name}:${var.docker_image_tag}",
+      memoryReservation = 128,
+      essential = true,
+
+      environment = [for k, v in local.environment : {name = k, value = v}]
+      secrets = module.ssm.secrets
+
+      portMappings = [{
+       containerPort = var.docker_container_port,
+       // In case of bridge an host use a dynamid port (0)
+       hostPort = var.ecs_network_mode == "awsvpc" ? var.docker_container_port : 0
+      }]
+
+      volumeMappings = var.ecs_launch_type == "FARGATE" ? [] : [
+        {
+          containerVolume = "/var/run/docker.sock",
+          hostVolume = "/var/run/docker.sock"
+        }
+      ],
+
+      logConfiguration = var.cloudwatch_log_group == "" ? {
+        logDriver = "json-file"
+        options = {}
+      } : {
+        logDriver = "awslogs",
+        options = {
+          awslogs-group = var.cloudwatch_log_group
+          awslogs-region = data.aws_region.current.name
+          awslogs-stream-prefix = var.name
+        }
+      }
+    }
+
+
+}
+
+module "ssm" {
+  source = "hazelops/ssm-secrets/aws"
+  version = "~> 1.0"
+  env = var.env
+  app_name = var.app_name
+  names = local.secret_names
+}
---
+++ variables.tf
@@ -0,0 +1,50 @@
+variable "env" {
+}
+
+variable "name" {
+  default = "cloud9"
+}
+
+variable "app_name" {
+  type = string
+}
+
+
+variable "environment" {
+  type = map(string)
+  default = {}
+}
+
+variable "secret_names" {
+  type = list(string)
+  default = []
+}
+
+//variable "ecs_cluster" {
+//  type = string
+//}
+
+variable "docker_image_name" {
+  type = string
+  default = "linuxserver/cloud9"
+}
+
+variable "docker_image_tag" {
+  type = string
+  default = "latest"
+}
+
+variable "ecs_launch_type" {
+
+}
+
+variable "cloudwatch_log_group" {
+  default = ""
+}
+
+variable "docker_container_port" {
+  default = 8000
+}
+
+variable "ecs_network_mode" {
+}
